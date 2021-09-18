f042b23230bfba79bbe2767ad54e62457a653d80
armannm <anm4500@gmail.com>
2021-09-18 08:40:23 +0430

    changed structure

---
+++ playground/src/app/ngrx-store/ngrx.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { counterReducer } from './reducers/counter.reducer';
+import { environment } from 'src/environments/environment';
+
+export const routes: Routes = [{ path: '', component: CounterComponent }];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+  ],
+  exports: [],
+  declarations: [CounterComponent],
+  providers: [],
+})
+export class NgrxModule {}
