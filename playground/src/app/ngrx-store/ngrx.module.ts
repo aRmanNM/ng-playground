3cf5c309867c9ffd8cfc5b057f386d1980aab726
arman naeimi <anm4500@gmail.com>
2022-03-17 12:38:05 +0330

    updated readme and fixed redux devtool not working

--- playground/src/app/ngrx-store/ngrx.module.ts
+++ playground/src/app/ngrx-store/ngrx.module.ts
@@ -17,8 +17,6 @@ export const routes: Routes = [{ path: '', component: CounterComponent }];
     StoreModule.forRoot({ count: counterReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
     }),
   ],
   exports: [],
